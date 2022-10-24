dfec29d374e31f7862164d4e2d0a49195e3898b7
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-10-23 21:06:34 -0400

    deleted comments

--- app/routes.py
+++ app/routes.py
@@ -18,7 +18,8 @@ planets = [
 bp = Blueprint("planets", __name__, url_prefix="/planets")  # ENDPOINT
 
 
-@bp.route("", methods=["GET"])  # CRUD METHOD created a new endpoint that catches requests going to "" (assumed
+# CRUD METHOD created a new endpoint that catches requests going to "" (assumed
+@bp.route("", methods=["GET"])
 # "/books") with the HTTP method GET
 # RESPONSE BODY
 def handle_planets():
@@ -31,31 +32,15 @@ def handle_planets():
         ))
     return jsonify(results_list)
 
-#from flask import Blueprint, jsonify
-
-# class Cat:
-#     def __init__(self, id, name, color, personality):
-#         self.id = id
-#         self.name = name
-#         self.color = color
-#         self.personality = personality
-
-# cats = [
-#     Cat(1, "Luna", "grey", "naughty"),
-#     Cat(2, "Orange Cat", "orange", "antagonistic"),
-#     Cat(3, "Big Ears", "grey and white", "sleepy")
-# ]
-
-# bp = Blueprint("cats", __name__, url_prefix="/cats")
-
-# @bp.route("", methods=["GET"])
-# def handle_cats():
-#     results_list = []
-#     for cat in cats:
-#         results_list.append(dict(
-#             id=cat.id,
-#             name=cat.name,
-#             color=cat.color,
-#             personality=cat.personality
-#         ))
-#     return jsonify(results_list)
+@bp.route("/<planet_id>", methods=['GET'])
+def handle_planet(planet_id):
+    planet_id = int(planet_id)
+    for planet in planets:
+        if planet.id == planet_id:
+            return {
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "temperature": planet.temperature
+            }
+    pass

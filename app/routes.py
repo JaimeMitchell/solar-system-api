28c1426e0a415fd5106025eccfe9a30c81ff9568
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-10-23 21:27:34 -0400

    Finished Planet 400 and 404

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 
 class Planet:
@@ -33,9 +33,20 @@ def handle_planets():
     return jsonify(results_list)
 
 
-@bp.route("/<planet_id>", methods=['GET'])
-def handle_planet(planet_id):
-    planet_id = int(planet_id)
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response(
+            {"message": f"planet {planet_id} is not valid"}, 400))
     for planet in planets:
         if planet.id == planet_id:
-            return dict(id=planet.id, name=planet.name, temperature=planet.temperature)
+            return(planet)
+    abort(make_response(
+        {"message": f"planet {planet_id} does not exist"}, 404))
+
+
+@bp.route("/<planet_id>", methods=['GET'])
+def handle_planet(planet_id):
+    planet = validate_planet(planet_id)
+    return dict(id=planet.id, name=planet.name, temperature=planet.temperature)

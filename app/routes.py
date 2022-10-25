f23447a8e06252055d74cfc48f27f096c2db70b6
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-10-24 21:05:59 -0400

    fixing some errors

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,7 @@ class Planet:
         self.name = name
         self.description = description
         self.temperature = temperature
+
     def to_planet_dict(self):
         return dict(
             id=self.id,
@@ -23,29 +24,29 @@ planets = [
     Planet(3, "Mars", "red", "300F")
 ]
 
-bp = Blueprint("planets", __name__, url_prefix="/planets") 
+bp = Blueprint("planets", __name__, url_prefix="/planets")
+
 
 def validate_planet(planet_id):
     try:
         planet_id = int(planet_id)
     except:
-        abort(make_response({"message":f"planet{planet_id} not valid"},400))
+        abort(make_response({"message": f"planet {planet_id} not valid"}, 400))
     for planet in planets:
         if planet.id == planet_id:
             return planet
-    return abort(make_response({"message": f"planet{planet_id} not found"}, 404))
-    
+    abort(make_response({"message": f"planet {planet_id} not found"}, 404))
+
 
 @bp.route("", methods=["GET"])
-
 def handle_planets():
     results_list = []
     for planet in planets:
         results_list.append(planet.to_planet_dict())
     return jsonify(results_list)
-    
+
+
 @bp.route("/<planet_id>", methods=["GET"])
 def get_planet(planet_id):
     planet = validate_planet(planet_id)
     return jsonify(planet.to_planet_dict())
-

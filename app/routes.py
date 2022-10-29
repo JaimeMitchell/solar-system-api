37407398862c4865e96fccfcc840c4a90b7164e5
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-10-28 22:47:03 -0400

    POST finito

--- app/routes.py
+++ app/routes.py
@@ -1,53 +1,18 @@
-from flask import Blueprint, jsonify, abort, make_response
-import json
+from app import db
+from app.models.planet import Planet
+from flask import Blueprint, jsonify, abort, make_response, request
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
 
-class Planet:
-    def __init__(self, id, name, description, temperature):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.temperature = temperature
-
-    def to_planet_dict(self):
-        return dict(
-            id=self.id,
-            name=self.name,
-            description=self.description,
-            temperature=self.temperature
-        )
-
-
-planets = [
-    Planet(1, 'Mercury', 'Grey', '333˚F(167˚C)'),
-    Planet(2, 'Venus', 'yellow-white', '967˚F(15°C)'),
-    Planet(3, 'Earth', 'blue-green', '59˚F(15°C)'),
-    Planet(4, 'Mars', 'red', '-85˚F(-65˚C')
-]
-
-bp = Blueprint("planets", __name__, url_prefix="/planets")
-
-
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
-        abort(make_response({"message": f"planet {planet_id} not valid"}, 400))
-    for planet in planets:
-        if planet.id == planet_id:
-            return planet
-    abort(make_response({"message": f"planet {planet_id} not found"}, 404))
-
-
-@bp.route("", methods=["GET"])
+# ("/",strict_slashes=False,methods=["POST"])
+@planets_bp.route("", methods=["POST"])
 def handle_planets():
-    results_list = []
-    for planet in planets:
-        results_list.append(planet.to_planet_dict())
-    return jsonify(results_list)
+    request_body = request.get_json()
+    new_planet = Planet(name=request_body["name"],
+                        description=request_body["description"],
+                        temperature=request_body["temperature"])
+    db.session.add(new_planet)
+    db.session.commit()
 
-
-@bp.route("/<planet_id>", methods=["GET"])
-def get_planet(planet_id):
-    planet = validate_planet(planet_id)
-    return jsonify(planet.to_planet_dict())
+    return make_response(f"Planet {new_planet.name} successful", 201)
--- solar_system.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-with open("planets.csv", "r", encoding="utf-8", newline="") as fid:
-    reader = csv.reader(fid, delimiter=",")
-    for planet in reader:
-        print(planet[0])

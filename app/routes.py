1e825cc9e0873e37bc28b655e1cec6ce4269f94b
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-10-29 20:40:37 -0400

    finished wave 3

--- app/routes.py
+++ app/routes.py
@@ -6,8 +6,9 @@ planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
 
 # ("/",strict_slashes=False,methods=["POST"])
-@planets_bp.route("", methods=["POST"])
-def handle_planets():
+# strict_slashes can use (or not) slash after endpoint!
+@planets_bp.route("/", strict_slashes=False, methods=["POST"])
+def create_planets():
     request_body = request.get_json()
     new_planet = Planet(name=request_body["name"],
                         description=request_body["description"],
@@ -16,3 +17,16 @@ def handle_planets():
     db.session.commit()
 
     return make_response(f"Planet {new_planet.name} successful", 201)
+
+@planets_bp.route("/", strict_slashes=False, methods=["GET"])
+def read_planets():
+    planets = Planet.query.all()
+    planets_response = []
+    for planet in planets:
+        planets_response.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "temperature": planet.temperature
+        })
+    return jsonify(planets_response)

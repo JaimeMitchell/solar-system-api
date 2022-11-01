3223563812228ef42ead83dd63ae8cd236e8803a
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-11-01 10:35:36 -0400

    finished read one planet

--- app/models/planet.py
+++ app/models/planet.py
@@ -1,8 +1,8 @@
 from app import db
 
-class Planet(db.Model): #The class Planet inherits from db.Model from SQLAlchemy
-    id= db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name= db.Column(db.String)
-    description = db.Column(db.String)
-    temperature = db.Column(db.Integer)
-    
+
+class Planet(db.Model):  # The class Planet inherits from db.Model from SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    temperature = db.Column(db.Integer, nullable=False)
--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,10 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-
 # ("/",strict_slashes=False,methods=["POST"])
 # strict_slashes can use (or not) slash after endpoint!
+
+
 @planets_bp.route("/", strict_slashes=False, methods=["POST"])
 def create_planets():
     request_body = request.get_json()
@@ -18,9 +19,10 @@ def create_planets():
 
     return make_response(f"Planet {new_planet.name} successful", 201)
 
+
 @planets_bp.route("/", strict_slashes=False, methods=["GET"])
-def read_planets():
-    planets = Planet.query.all()
+def handle_planets():
+    planets = Planet.query.all()  # response body from request
     planets_response = []
     for planet in planets:
         planets_response.append({
@@ -30,3 +32,25 @@ def read_planets():
             "temperature": planet.temperature
         })
     return jsonify(planets_response)
+
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message": f"{planet_id} is not valid"}, 400))
+    planet = Planet.query.get(planet_id)
+    if not planet:
+        abort(make_response({"message": f"{planet_id} can't be found"}, 404))
+    return planet  # this returns the query response body
+
+
+@planets_bp.route("/<planet_id>", strict_slashes=False, methods=["GET"])
+def read_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description,
+        "temperature": planet.temperature
+    }

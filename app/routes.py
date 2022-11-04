f2ba9c7e585d36c042202541df8a736dc14b5851
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-11-03 21:03:56 -0400

    finished wave_6 tests

--- app/routes.py
+++ app/routes.py
@@ -31,7 +31,7 @@ def create_planet():
     db.session.add(new_planet)
     db.session.commit()
 
-    return make_response(f"Planet #{new_planet.name} successfully created", 201)
+    return make_response(jsonify(f"Planet {new_planet.name} successfully created"), 201)
 
 
 @planets_bp.route("/", strict_slashes=False, methods=["GET"])
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,6 +6,14 @@ def test_get_all_planets_with_no_records(client):
     assert response.status_code == 200
     assert response_body == []
 
+def test_get_all_planets(client,two_saved_planets):
+     # Act
+    response = client.get("/planets")
+    response_body = response.get_json()
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 2
+
 
 def test_get_one_planet(client,two_saved_planets):
     # Act
@@ -19,3 +27,25 @@ def test_get_one_planet(client,two_saved_planets):
        "name":"Ocean Planet", 
        "description": "watr 4evr planet", 
        "temperature" : 1000}
+
+def test_get_one_planet_no_fixture(client):
+    # Act
+    response = client.get("/planets/1")
+
+    # Assert
+    assert response.status_code == 404
+
+def test_create_one_planet(client):
+    # Act
+    response = client.post("/planets", json={
+       "name":"Ocean Planet", 
+       "description": "watr 4evr planet", 
+       "temperature" : 1000}
+    )
+    response_body = response.get_json()
+
+    # Assert 
+    assert response.status_code == 201
+    assert response_body == "Planet Ocean Planet successfully created"
+
+# def test_update_one_planet

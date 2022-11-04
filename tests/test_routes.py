afc25e181c91258c587111ee4f1c7013f22450d9
Franklyn Rodriguez <franklynrodriguez.swe@gmail.com>
2022-11-03 22:02:03 -0400

    Wrapping up wave 6

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -16,6 +16,19 @@ def test_get_one_planet(client,two_saved_planets):
     assert response.status_code == 200
     assert response_body == {
         "id": 1,
-       "name":"Ocean Planet", 
-       "description": "watr 4evr planet", 
-       "temperature" : 1000}
+        "name":"Ocean Planet", 
+        "description": "watr 4evr planet", 
+        "temperature" : 1000}
+
+def test_create_one_planet(client):
+    # Act
+    response = client.post("/books", json={
+        "name":"Cheethra Planet", 
+        "description": "snow leopards", 
+        "temperature" : 10
+    })
+    response_body = response.get_data(as_text=True)
+
+    # Assert
+    assert response.status_code == 201
+    assert response_body == "Planet New Planet successfully created"
